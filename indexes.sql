f75451aaa6edcbd172090af648f59a13a0ed05ec
khoahuynhdev <khoa0319@gmail.com>
2023-11-21 22:07:23 +0700

    update

--- indexes.sql
+++ indexes.sql
@@ -5,3 +5,5 @@
 --
 -- index scan: scan the index but go to the table heap page to get the data
 -- index only scan: same as index scan but never touch the table heap page -> a lot faster
+-- composite index: indexes are built from left to right -> can use condition with a alone
+--  - if we have composite (a,b) and we use condition with b -> PG will fallback to use seq scan
